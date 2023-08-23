4a7a0bbd0bdd455df3744dece5692a4c950c9b25
Abdul Alim <abdulalim505011@gmail.com>
2023-08-23 21:17:14 +0600

    Add your commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,35 @@ $(document).ready(function () {
     $(".offcanvas-area , .offcanvas-overlay").removeClass("active");
   });
 
-  $(window).on("scroll", function () {
-    var scroll = $(window).scrollTop();
-    if (scroll < 5) {
-      $(".header").removeClass("scroll-header");
-    } else {
-      $(".header").addClass("scroll-header");
-    }
-  });
+$('.member__slider').owlCarousel({
+  items: 1,
+  slidesToShow: 1,
+  loop: true,
+  margin: 22,
+  nav: true,
+  navText: ['<img src="assets/img/arrow_angle.svg" alt="">','<img src="assets/img/arrow_angle.svg" alt="">'],
+  dot: false,
+  responsive: {
+    400: {
+        items: 1
+    },
+    500: {
+        items: 1
+    },
+    600: {
+        items: 2
+    },
+    1000: {
+        items: 3
+    },
+    
+    1200: {
+        items: 3
+    },
+    1300: {
+      items: 4
+  }
+}
+});
+
 });
